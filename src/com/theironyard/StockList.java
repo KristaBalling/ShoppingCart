fdc044d418a2fa35b5031ea40f4e2ebd01e7d0fe
Krista Blachian <krista.blachian@scifusion360.com>
2018-12-06 16:37:18 -0500

    unmodifiable lists and quantity adjustments

--- src/com/theironyard/StockList.java
+++ src/com/theironyard/StockList.java
@@ -40,7 +40,13 @@ public class StockList {
     public StockItem get(String key) {
         return list.get(key);
     }
-
+    public Map<String, Double> PriceList() {
+        Map<String, Double> prices = new LinkedHashMap<>();
+        for(Map.Entry<String, StockItem> item : list.entrySet()) {
+            prices.put(item.getKey(), item.getValue().getPrice());
+        }
+        return Collections.unmodifiableMap(prices);
+    }
     public Map<String, StockItem> Items(){
         return Collections.unmodifiableMap(list);
     }
